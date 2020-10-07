851069ba8613d5900f0a4dd56e668276928bf2bd
Daniel Mason <daniel@danielmason.com>
2020-10-07 17:58:41 +0100

    refactor: make description part of UvIndex with static lifetime to str

--- src/met_office/uv.rs
+++ src/met_office/uv.rs
@@ -4,11 +4,14 @@ use std::convert::TryFrom;
 type UvCode = str;
 
 #[derive(Debug, Eq, PartialEq)]
-struct UvIndex(u8);
+struct UvIndex {
+    index: u8,
+    description: &'static str,
+}
 
 impl UvIndex {
-    fn describe(&self) -> &str {
-        match self.0 {
+    fn index_to_description(index: u8) -> &'static str {
+        match index {
             0..=2 => "Low exposure. No protection required. You can safely stay outside",
             3..=5 => "Moderate exposure. Seek shade during midday hours, cover up and wear sunscreen",
             6..=7 => "High exposure. Seek shade during midday hours, cover up and wear sunscreen",
@@ -17,6 +20,7 @@ impl UvIndex {
         }
     }
 }
+
 
 #[derive(Error, Debug, PartialEq)]
 pub enum UvCodeConversionError {
@@ -29,7 +33,8 @@ impl TryFrom<&UvCode> for UvIndex {
 
     fn try_from(code: &UvCode) -> Result<Self, Self::Error> {
         let index: u8 = code.parse().map_err(|_| UvCodeConversionError::InvalidCode(code.to_string()))?;
-        Ok(UvIndex(index))
+        let description = UvIndex::index_to_description(index);
+        Ok(UvIndex { index, description })
     }
 }
 
@@ -39,18 +44,30 @@ mod tests {
 
     #[test]
     fn test_uv_code_to_index() {
-        assert_eq!(UvIndex::try_from("1").unwrap(), UvIndex(1));
-        assert_eq!(UvIndex::try_from("2").unwrap(), UvIndex(2));
-        assert_eq!(UvIndex::try_from("3").unwrap(), UvIndex(3));
-        assert_eq!(UvIndex::try_from("4").unwrap(), UvIndex(4));
-        assert_eq!(UvIndex::try_from("5").unwrap(), UvIndex(5));
-        assert_eq!(UvIndex::try_from("6").unwrap(), UvIndex(6));
-        assert_eq!(UvIndex::try_from("7").unwrap(), UvIndex(7));
-        assert_eq!(UvIndex::try_from("8").unwrap(), UvIndex(8));
-        assert_eq!(UvIndex::try_from("9").unwrap(), UvIndex(9));
-        assert_eq!(UvIndex::try_from("10").unwrap(), UvIndex(10));
-        assert_eq!(UvIndex::try_from("11").unwrap(), UvIndex(11));
-        assert_eq!(UvIndex::try_from("255").unwrap(), UvIndex(255));
+        assert_eq!(UvIndex::try_from("1").unwrap().index, 1);
+        assert_eq!(UvIndex::try_from("1").unwrap().description, "Low exposure. No protection required. You can safely stay outside");
+        assert_eq!(UvIndex::try_from("2").unwrap().index, 2);
+        assert_eq!(UvIndex::try_from("2").unwrap().description, "Low exposure. No protection required. You can safely stay outside");
+        assert_eq!(UvIndex::try_from("3").unwrap().index, 3);
+        assert_eq!(UvIndex::try_from("3").unwrap().description, "Moderate exposure. Seek shade during midday hours, cover up and wear sunscreen");
+        assert_eq!(UvIndex::try_from("4").unwrap().index, 4);
+        assert_eq!(UvIndex::try_from("4").unwrap().description, "Moderate exposure. Seek shade during midday hours, cover up and wear sunscreen");
+        assert_eq!(UvIndex::try_from("5").unwrap().index, 5);
+        assert_eq!(UvIndex::try_from("5").unwrap().description, "Moderate exposure. Seek shade during midday hours, cover up and wear sunscreen");
+        assert_eq!(UvIndex::try_from("6").unwrap().index, 6);
+        assert_eq!(UvIndex::try_from("6").unwrap().description, "High exposure. Seek shade during midday hours, cover up and wear sunscreen");
+        assert_eq!(UvIndex::try_from("7").unwrap().index, 7);
+        assert_eq!(UvIndex::try_from("7").unwrap().description, "High exposure. Seek shade during midday hours, cover up and wear sunscreen");
+        assert_eq!(UvIndex::try_from("8").unwrap().index, 8);
+        assert_eq!(UvIndex::try_from("8").unwrap().description, "Very high. Avoid being outside during midday hours. Shirt, sunscreen and hat are essential");
+        assert_eq!(UvIndex::try_from("9").unwrap().index, 9);
+        assert_eq!(UvIndex::try_from("9").unwrap().description, "Very high. Avoid being outside during midday hours. Shirt, sunscreen and hat are essential");
+        assert_eq!(UvIndex::try_from("10").unwrap().index, 10);
+        assert_eq!(UvIndex::try_from("10").unwrap().description, "Very high. Avoid being outside during midday hours. Shirt, sunscreen and hat are essential");
+        assert_eq!(UvIndex::try_from("11").unwrap().index, 11);
+        assert_eq!(UvIndex::try_from("11").unwrap().description, "Extreme. Avoid being outside during midday hours. Shirt, sunscreen and hat essential.");
+        assert_eq!(UvIndex::try_from("255").unwrap().index, 255);
+        assert_eq!(UvIndex::try_from("255").unwrap().description, "Extreme. Avoid being outside during midday hours. Shirt, sunscreen and hat essential.");
     }
 
     #[test]
